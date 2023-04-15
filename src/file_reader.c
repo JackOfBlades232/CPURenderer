54485af249c0b614ef318d058bccb601df0994dc
JackOfBlades232 <petrkiyashko232@gmail.com>
2023-04-15 21:35:31 +0300

    Fixed vertex normals

--- src/file_reader.c
+++ src/file_reader.c
@@ -453,12 +453,11 @@ static scene_obj triangle_from_vertex_infos(vertex_info *v1,
             v3->v->x, v3->v->y, v3->v->z
             );
 
-    // @TODO Should i normalize vn-s here?
     if (v1->vn && v2->vn && v3->vn) {
         tr.data.tr.has_vn = 1;
-        tr.data.tr.vn1 = vec3d_normalized(*(v1->v));
-        tr.data.tr.vn2 = vec3d_normalized(*(v2->v));
-        tr.data.tr.vn3 = vec3d_normalized(*(v3->v));
+        tr.data.tr.vn1 = vec3d_normalized(*(v1->vn));
+        tr.data.tr.vn2 = vec3d_normalized(*(v2->vn));
+        tr.data.tr.vn3 = vec3d_normalized(*(v3->vn));
     } 
     
     return tr;
